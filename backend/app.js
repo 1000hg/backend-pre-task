66a70ad142ed8f3ea0d3c5b70c9c791950c29b48
yskim <alxzkf123@gmail.com>
2023-06-08 00:37:23 +0900

    [Chore] 환경설정 구축

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const db = require("./models");
 
 const routes = require('./routes');
+
+
+db.sequelize.sync();
 
 app.use('/', routes);
 
