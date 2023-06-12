2e07714a4a233f51e2b110459a2b55c11ccc8c8c
yskim <alxzkf123@gmail.com>
2023-06-12 22:57:18 +0900

    [chore] 로컬 구동 방식 변경

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require("./models");
+const cors = require('cors');
+
 
 const routes = require('./routes');
 
 
 //db.sequelize.sync();
+
+app.use(cors()); 
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 
 app.use(bodyParser.json());
 app.use('/', routes);
